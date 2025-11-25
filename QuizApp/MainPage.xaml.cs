772d1e942ae21869084b4af43df79ea58b20177c
student <student@student>
2025-11-25 11:22:28 +0100

    Ended 1 screen.

--- QuizApp/MainPage.xaml.cs
+++ QuizApp/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿namespace QuizApp
+﻿
+namespace QuizApp
 {
     public partial class MainPage : ContentPage
     {
@@ -6,6 +7,34 @@
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private async void StartGame_Clicked(object sender, EventArgs e)
+        {
+            string FirstPlayer = FirstPlayerLabel.Text;
+            string SecondPlayer = SecondPlayerLabel.Text;
+            int QuestionsNumber;
+            if (int.TryParse(QuestionsNumberLabel.Text, out QuestionsNumber))
+            {
+                if(QuestionsNumber >=10 && QuestionsNumber <= 15)
+                {
+                    if (FirstPlayer != null && SecondPlayer != null && !(FirstPlayer == SecondPlayer))
+                    {
+                        await Navigation.PushAsync(new QuizPage());
+                    }
+                    else
+                    {
+                        await DisplayAlert("Podaj poprawne nazwy graczy.", "Spróbuj ponownie", "Ok");
+                    }
+                }
+                else
+                {
+                    await DisplayAlert("Podaj poprawną ilość pytań.", "Spróbuj ponownie", "Ok");
+                }
+            }
+            else {
+                await DisplayAlert("Podaj poprawną ilość pytań.", "Spróbuj ponownie", "Ok");
+            }
         }
     }
 }
